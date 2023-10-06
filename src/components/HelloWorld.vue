88c83d0770ece1ad1b35aefbaadaa53928e202d1
Delsea12 <dellasetyowati12@gmail.com>
2023-10-06 16:20:59 +0700

    fe

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup>
+// import { ref } from 'vue'
+
+// defineProps({
+//   msg: String,
+// })
+
+// const count = ref(0)
+</script>
+
+<template>
+ 
+<div class="py-10 px-20 ">Network Traficc Analysis</div> 
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
