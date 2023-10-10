2f02e636f5fb162e41be1346a0ad4d8e71417456
gunggus665 <gunggus665@gmail.com>
2023-10-10 11:26:25 +0700

    feat: list rendering

--- frontend/src/components/TableView.vue
+++ frontend/src/components/TableView.vue
@@ -1,48 +1,45 @@
 <script setup>
-const times = ["08:00 AM", "09:15 AM", "10:30 AM", "11:45 AM"];
-const logs = ["Log 1", "Log 2", "Log 3", "Log 4"];
-const threatLevels = ["Low", "Medium", "High"];
-const detail = ["Detail 1", "Detail 2", "Detail 3"];
+const data = [
+  {
+    timestamp : "fadfads",
+    src_ip : "fadfads",
+    src_port : "fadfads",
+    dst_ip : "fadfads",
+    dst_port : "fadfads",
+    detail : "fadfads",
+  }
+]
 </script>
 
 <template>
   <div class="bg-[#FAF0F0] w-[1260px] mx-[117px] my-[10px] text-[#1F3C9C]">
-    <div class="grid grid-cols-4 gap-4">
-      <div>
+    <div class="grid grid-cols-6 gap-4">
         <div class="font-bold">Timestamp</div>
-        <div v-for="(time, index) in times" :key="'time-' + index" class="py-2">
-          {{ time }}
-        </div>
-      </div>
-
-      <div>
-        <div class="font-bold">Log</div>
-        <div v-for="(log, index) in logs" :key="'log-' + index" class="py-2">
-          {{ log }}
-        </div>
-      </div>
-
-      <div>
-        <div class="font-bold">Threats Level</div>
-        <div
-          v-for="(threatLevel, index) in threatLevels"
-          :key="'threat-' + index"
-          class="py-2"
-        >
-          {{ threatLevel }}
-        </div>
-      </div>
-
-      <div>
+        <div class="font-bold">Source IP</div>
+        <div class="font-bold">Source Port</div>
+        <div class="font-bold">Destination IP</div>
+        <div class="font-bold">Destination PORT</div>
         <div class="font-bold">Detail</div>
-        <div
-          v-for="(detailItem, index) in detail"
-          :key="'detail-' + index"
-          class="py-2"
-        >
-          {{ detailItem }}
-        </div>
-      </div>
+        <template v-for="(d) in data">
+          <div >
+            {{ d.timestamp }}
+          </div>
+           <div>
+            {{ d.src_ip }}
+          </div>
+           <div>
+            {{ d.src_port }}
+          </div>
+           <div>
+            {{ d.dst_ip }}
+          </div>
+           <div>
+            {{ d.dst_port }}
+          </div>
+          <div>
+            {{ d.detail }}
+          </div>
+        </template>
     </div>
   </div>
 </template>
